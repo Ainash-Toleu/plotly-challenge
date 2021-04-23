d7c9a08be89cb042531ec350562ce93ccc29be6e
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-04-23 14:13:24 -0500

    made minor changes by moving folders and files

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,15 @@
+console.log('app.js loaded')
+
+function InitDashboard () {
+    console.log("InitDashboard()");
+    // Populate the dropdown
+    var selector = d3.select("#selDataset");
+    d3.json("data/samples.json").then(function(data){
+        console.log(data); 
+    });
+    // Update the bargraph
+    // Update the bubblechart
+    // Update the demographic information
+}
+
+InitDashboard();
